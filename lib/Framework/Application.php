08062c497510c81193d6d5baa89c3004c62339ba
David Baudry <david.baudry@zoho.com>
2017-06-05 16:19:00 +0200

    Class Application in framework

---
+++ lib/Framework/Application.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: David
+ * Date: 05/06/2017
+ * Time: 15:37
+ * --
+ * This is the abstract main class application, to describe how applications will run (frontend and backend)
+ *
+ */
+
+namespace Framework;
+
+
+abstract class Application
+{
+    protected $HTTPRequest;
+    protected $HTTPResponse;
+    protected $name;
+
+    public function __construct()
+    {
+        $this->httpRequest = new HTTPRequest;
+        $this->httpResponse = new HTTPResponse;
+        $this->name = '';
+    }
+
+    abstract public function run() :void;
+
+    /*
+     * GETTERS
+     */
+
+    public function getHttpRequest()
+    {
+        return $this->httpRequest;
+    }
+
+    public function getHttpResponse()
+    {
+        return $this->httpResponse;
+    }
+
+    public function getName() :string {
+        return $this->name;
+    }
+}
