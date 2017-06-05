22e4180f65ba25ed6ed5d2a1853ed8f458cfc887
David Baudry <david.baudry@zoho.com>
2017-06-05 15:59:50 +0200

    Class HTTPRequest in framework

---
+++ lib/Framework/HTTPRequest.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: David
+ * Date: 05/06/2017
+ * Time: 15:41
+ */
+
+namespace Framework;
+
+class HTTPRequest
+{
+    /*
+     * Cookies
+     */
+
+    public function cookieExists(string $key) :bool
+    {
+        if (isset($_COOKIE[$key])) {
+            return true;
+        }
+        return false;
+    }
+
+    public function cookieData(string $key) :string
+    {
+        if ($this->cookieExists($key)) {
+            return $_COOKIE[$key];
+        }
+        return null;
+    }
+
+    /*
+     * Retrieve Request Method and URI
+     */
+
+    public function method() :string
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+
+    public function requestURI()
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
+
+    /*
+     * GET
+     */
+
+    public function getExists(string $key) :bool
+    {
+        if (isset($_GET[$key])) {
+            return true;
+        }
+        return false;
+    }
+
+    public function getData(string $key) :string
+    {
+        if ($this->getExists($key)) {
+            return $_GET[$key];
+        }
+        return null;
+    }
+
+    /*
+     * POST
+     */
+
+    public function postExists(string $key) :bool
+    {
+        if (isset($_POST[$key])) {
+            return true;
+        }
+        return false;
+    }
+
+    public function postData(string $key) :string
+    {
+        if ($this->postExists($key)) {
+            return $_POST[$key];
+        }
+        return null;
+    }
+
+
+
+
+}
